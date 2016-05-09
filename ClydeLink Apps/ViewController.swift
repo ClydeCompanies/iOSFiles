57e96a2d6546e03641e3703333b0711b48448031
jfed8 <jadenfeddock@gmail.com>
2016-05-09 14:51:43 -0700

    Second Commit - Testing

--- ClydeLink Apps/ViewController.swift
+++ ClydeLink Apps/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Testing")
+        print("Jackson")
     }
 
     override func didReceiveMemoryWarning() {
