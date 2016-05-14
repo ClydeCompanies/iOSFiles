915293a362ec1d58becb1eed652af64ecb9a06d8
jfed8 <jadenfeddock@gmail.com>
2016-05-14 09:48:13 -0700

    Added Core Data

---
+++ ClydeLink Apps/AppsTable.swift
@@ -0,0 +1,24 @@
+//
+//  AppsTable.swift
+//  ClydeLink Apps
+//
+//  Created by J J Feddock on 5/14/16.
+//  Copyright © 2016 XLR8 Development LLC. All rights reserved.
+//
+
+import UIKit
+
+class AppsTable: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+
+}
