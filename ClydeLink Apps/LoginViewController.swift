ac905a4f7d6da9e10fea44b9b5289922cb3a5534
jfed8 <jadenfeddock@gmail.com>
2016-05-14 09:22:38 -0700

    Login Screen Basics

---
+++ ClydeLink Apps/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  ClydeLink Apps
+//
+//  Created by J J Feddock on 5/14/16.
+//  Copyright © 2016 XLR8 Development LLC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func LoginButtonPress(sender: AnyObject) {
+        let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Home")
+        self.showViewController(vc as! UIViewController, sender: vc)
+    }
+    
+    @IBOutlet weak var Username: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    
+    
+}
