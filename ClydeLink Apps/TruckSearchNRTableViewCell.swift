412e56c41dc95378317df2d4e72c1d7976c8cea4
Jaden Feddock <jadenfeddock@gmail.com>
2016-05-31 16:36:53 -0700

    Add files via upload

---
+++ ClydeLink Apps/TruckSearchNRTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TruckSearchTableViewCell.swift
+//  ClydeLink Apps
+//
+//  Created by XLR8 Development LLC on 5/19/16.
+//
+
+import UIKit
+
+class TruckSearchNRTableViewCell: UITableViewCell {  // Controls the content of each cell that is added to the Results Table from the Truck Search
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+
+}
