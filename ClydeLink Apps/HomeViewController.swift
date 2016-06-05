7808d2a15bf94c133596b382b0806c26240c6c80
Jackson William Feddock <JFed@jacksons-macbook-air.local>
2016-06-04 22:02:49 -0700

    Preliminary edits on the edit screen -Jackson

--- ClydeLink Apps/HomeViewController.swift
+++ ClydeLink Apps/HomeViewController.swift
@@ -12,6 +12,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var leftButton: UIBarButtonItem!
     
+    @IBOutlet weak var rightButton: UIBarButtonItem!
+    
     @IBOutlet weak var AppTable: UITableView!
     
     var appButtons: Array = [App]()
@@ -66,7 +68,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {  // Returns length of all the buttons needed
-        loadChecked()
         
         return self.appButtons.count
     }
@@ -83,11 +84,71 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             let cell = self.AppTable.dequeueReusableCellWithIdentifier("AppCell", forIndexPath: indexPath) as! AppTableViewCell
             
             cell.Title.text = self.appButtons[AppCount].title
-            
             AppCount += 1;
             return cell
         
     }
+    
+    //************************************************************************************************
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            for element in currentapps
+            {
+                if (element.title == appButtons[indexPath.row].title)
+                {
+                    element.selected = false
+                    break
+                }
+            }
+//            prefs.setObject(currentapps, forKey: "userapps")
+//            prefs.synchronize()
+            appButtons.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        if self.AppTable.editing {return .Delete}
+        return .None
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        let itemToMove = appButtons[fromIndexPath.row]
+        appButtons.removeAtIndex(fromIndexPath.row)
+        appButtons.insert(itemToMove, atIndex: toIndexPath.row)
+        var fromindex: Int = 0
+        for element in currentapps
+        {
+            if (element.title == itemToMove.title) {
+                fromindex = currentapps.indexOf(element)!
+            }
+        }
+        
+        var toindex: Int = 0
+        
+        for element in currentapps
+        {
+            if (element.title == appButtons[fromIndexPath.row + 1].title) {
+                toindex = currentapps.indexOf(element)!
+            }
+        }
+        currentapps.removeAtIndex(fromindex)
+        currentapps.insert(itemToMove, atIndex: toindex + 1)
+        print("*****")
+        for element in currentapps
+        {
+            print(element.title + " " + String(element.selected))
+        }
+//        prefs.setObject(currentapps, forKey: "userapps")
+//        prefs.synchronize()
+    }
+    
+    //********************************************************************************************************
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {  // Gives the height for each row
         return 60.0
@@ -116,21 +177,30 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     @IBAction func editTable(sender: AnyObject) {  // Edit button pressed
-        let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Edit")
-        self.presentViewController(vc as! UIViewController, animated: false, completion: nil)
-        
+        if (leftButton.title == "Edit")
+        {
+            AppTable.setEditing(true,animated: true)
+            leftButton.title = "Done"
+            rightButton.title = "Add"
+        } else if (leftButton.title == "Done")
+        {
+            AppTable.setEditing(false,animated: true)
+            leftButton.title = "Edit"
+            rightButton.title = "Settings"
+        }
     }
     
     
     @IBAction func settingsButton(sender: AnyObject) {  // Settings button pressed
-        let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Settings")
-        self.presentViewController(vc as! UIViewController, animated: false, completion: nil)
-        
-    }
-    
-    @IBAction func AddFeatures(sender: AnyObject) {
-        let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Add Features")
-        self.presentViewController(vc as! UIViewController, animated: false, completion: nil)
+        if (rightButton.title == "Settings")
+        {
+            let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Settings")
+            self.presentViewController(vc as! UIViewController, animated: false, completion: nil)
+        } else if (rightButton.title == "Add")
+        {
+            let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Add Features")
+            self.presentViewController(vc as! UIViewController, animated: false, completion: nil)
+        }
     }
     
     func loadChecked()
@@ -145,61 +215,65 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func fillAppArray(inout currentapps: [App])
     {
+        let debug = true
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Accounting and Credit Apps"
         currentapps[currentapps.count - 1].title = "Create New Customer Account"
         currentapps[currentapps.count - 1].link = "newcustomeraccount"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Accounting and Credit Apps"
         currentapps[currentapps.count - 1].title = "Credit Dashboard"
         currentapps[currentapps.count - 1].link = "creditdashboard"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Employee Apps"
         currentapps[currentapps.count - 1].title = "New Hire"
         currentapps[currentapps.count - 1].link = "newhire"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Employee Apps"
         currentapps[currentapps.count - 1].title = "Expense Reimbursement"
         currentapps[currentapps.count - 1].link = "expensereimbursement"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Equipment Apps"
         currentapps[currentapps.count - 1].title = "Vehicle Search"
         currentapps[currentapps.count - 1].link = "vehiclesearch"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true  // Debug
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Equipment Apps"
         currentapps[currentapps.count - 1].title = "Equipment Search"
         currentapps[currentapps.count - 1].link = "equipmentsearch"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Human Resources Apps"
         currentapps[currentapps.count - 1].title = "Training Request Form"
         currentapps[currentapps.count - 1].link = "trainingrequestform"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
         
         currentapps.append(App())
         currentapps[currentapps.count - 1].header = "Human Resources Apps"
         currentapps[currentapps.count - 1].title = "Employee Directory"
         currentapps[currentapps.count - 1].link = "employeedirectory"
         currentapps[currentapps.count - 1].permissions = 4
-        currentapps[currentapps.count - 1].selected = true
+        currentapps[currentapps.count - 1].selected = debug
+        //Convert function
+//        prefs.setObject(currentapps, forKey: "userapps")
+//        prefs.synchronize()
     }
     
     
