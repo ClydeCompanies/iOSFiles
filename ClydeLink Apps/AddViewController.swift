c178e51cf2f5ef99f5d5e6bfa6578939dd8e4944
jfed8 <jadenfeddock@gmail.com>
2018-09-11 20:01:32 -0700

    Logged In user

--- ClydeLink Apps/AddViewController.swift
+++ ClydeLink Apps/AddViewController.swift
@@ -312,7 +312,7 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         var uName: String = ""
         if (prefs.string(forKey: "username") != nil && prefs.string(forKey: "username") != "")
         {
-            uName = "Logged in as " + prefs.string(forKey: "LogInUser")!
+            uName = "Logged in as " + prefs.string(forKey: "username")!
         } else {
             uName = "Not logged in"
         }
